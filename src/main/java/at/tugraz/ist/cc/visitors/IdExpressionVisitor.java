bac0a84607dfdd2b13d9707537f8e0e710c47d5a
NaidaNozic <naida.nozic@reeinvent.com>
2024-05-11 19:04:55 +0200

    revertt

--- src/main/java/at/tugraz/ist/cc/visitors/IdExpressionVisitor.java
+++ src/main/java/at/tugraz/ist/cc/visitors/IdExpressionVisitor.java
@@ -43,7 +43,6 @@ public class IdExpressionVisitor extends JovaBaseVisitor<IdExpression> {
         if(leftExprOfDotOperator != null && !invalidDotOperatorRightExpr) {
             checkExpressionWithDotOperator(idExpression, method_symbol_table);
         } else if(invalidDotOperatorRightExpr) {
-            semanticErrors.add(new MemberExpectedError(idExpression.line));
             idExpression.type = "invalid";
         }else{
             checkExpression(idExpression, method_symbol_table);
